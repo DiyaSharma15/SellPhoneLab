2a1c65fedd0fe7681bef9e8bfad6ced5b511ed97
DiyaSharma15 <harry.diya161@gmail.com>
2024-02-13 17:01:09 -0700

    worked on Home Page and Header

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import Header from "./components/Header";
+import BrandToRepair from "./components/BrandToRepair";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Header/>
-      
+      <BrandToRepair/>
 
     </div>
   );
