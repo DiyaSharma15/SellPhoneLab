cacd745a9825f512a4998489629b9be9e6143f0c
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-02-13 19:44:52 -0700

    Started work on repair page

---
+++ app/repair/deviceData.js
@@ -0,0 +1,18 @@
+//repair/deviceData.js
+
+const deviceTypes = [
+    {
+        brand: Apple,
+        models: [
+            {
+                name: "iPhone",
+                devices: [
+                    {name: "iPhone 15 Pro Max", /*TODO: Add image*/},
+                    {name: "iPhone 15 Pro", /*TODO: Add image*/},
+                    {name: "iPhone 15", /*TODO: Add image*/},
+                    
+                ]
+            }
+        ]
+    }
+]
