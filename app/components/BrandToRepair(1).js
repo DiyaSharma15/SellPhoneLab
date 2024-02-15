d232a41a18d5575fd1c8adeedaeccb707a976672
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-02-14 21:13:25 -0700

    Uhh i didnt update anytthing but its 45 changed

---
+++ app/components/BrandToRepair(1).js
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../cssFiles/BrandToRepair.module.css"; 
+
+export default function BrandToRepair(){
+    return(
+        <div className={styles.container}>
+            <div className={styles.heading}>Choose Your Brand to Repair </div>
+            <ul className={styles.brandList}>
+                <li className={styles.brandItem}>
+                    <Link href="/">
+                        <img src="\assets\images\home\Logo-Apple.png" alt="Apple Logo" className={styles.logo}/>
+                    </Link>
+                </li>
+                <li className={styles.brandItem}>
+                    <Link href="/">
+                        <img src="\assets\images\home\Logo-Samsung.png" alt="Samsung Logo" className={styles.logo}/>
+                    </Link>
+                </li>
+                <li className={styles.brandItem}>
+                    <Link  href="/">
+                        <img src="\assets\images\home\Logo-Google.png" alt="Google Logo" className={styles.logo}/>
+                    </Link>
+                </li>
+                <li className={styles.brandItem}>
+                    <Link href="/">
+                        <img src="\assets\images\home\Logo-LG.png" alt="LG Logo" className={styles.logo}/>
+                    </Link>
+                </li>
+                <li className={styles.brandItem}>
+                    <Link href="/">
+                        <img src="\assets\images\home\Logo-Huawei.png" alt="Huawei Logo" className={styles.logo}/>
+                    </Link>
+                </li>
+                <li className={styles.brandItem}>
+                    <Link href="/">
+                        <img src="\assets\images\home\other-phones-2.jpg" alt="Other Phone Brand" className={styles.logo}/>
+                    </Link>
+                </li>
+            </ul>
+        </div>  
+    );
+}
---
+++ app/repair/deviceData(1).js
@@ -0,0 +1,170 @@
+//repair/deviceData.js
+
+const deviceTypes = [
+    {
+      brand: "Apple",
+      image: "/assets/images/repair/Apple/Apple-Logo.svg",
+      models: [
+        {
+          name: "iPhone",
+          image: "/assets/images/repair/Apple/iPhone/Apple-iPhone.png",
+          series: [
+            {
+              name: "iPhone 15",
+              image: "/assets/images/repair/Apple/iPhone/15/15ProMax.png",
+              devices: [
+                { name: "iPhone 15 Pro Max", image: "/assets/images/repair/Apple/iPhone/15ProMax.png"}, //TODO: Add image
+                { name: "iPhone 15 Pro" }, //TODO: Add image
+                { name: "iPhone 15 Plus" }, //TODO: Add image
+                { name: "iPhone 15" } //TODO: Add image
+              ]
+            },
+            {
+              name: "iPhone 14",
+              image: "/assets/images/repair/Apple/iPhone/14/14ProMax.png",
+              devices: [
+                { name: "iPhone 14 Pro Max" }, //TODO: Add image
+                { name: "iPhone 14 Pro" }, //TODO: Add image
+                { name: "iPhone 14 Plus" }, //TODO: Add image
+                { name: "iPhone 14" } //TODO: Add image
+              ]
+            },
+            {
+                name: "iPhone 13",
+                image: "/assets/images/repair/Apple/iPhone/13/13ProMax.png",
+                devices: [
+                    { name: "iPhone 13 Pro Max"}, //TODO: Add image
+                    { name: "iPhone 13 Pro" }, //TODO: Add image
+                    { name: "iPhone 13 mini" }, //TODO: Add image
+                    { name: "iPhone 13" } //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone 12",
+                image: "/assets/images/repair/Apple/iPhone/12/12ProMax.png",
+                devices: [
+                    { name: "iPhone 12 Pro Max" }, //TODO: Add image
+                    { name: "iPhone 12 Pro" }, //TODO: Add image
+                    { name: "iPhone 12 mini" }, //TODO: Add image
+                    { name: "iPhone 12" } //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone 11",
+                image: "/assets/images/repair/Apple/iPhone/11/11ProMax.png",
+                devices: [
+                    { name: "iPhone 11 Pro Max" }, //TODO: Add image
+                    { name: "iPhone 11 Pro" }, //TODO: Add image
+                    { name: "iPhone 11" } //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone X",
+                image: "/assets/images/repair/Apple/iPhone/X/iPhone-X.png",
+                devices: [
+                    { name: "iPhone X" }, //TODO: Add image
+                    { name: "iPhone XS" }, //TODO: Add image
+                    { name: "iPhone XR" }, //TODO: Add image
+                    { name: "iPhone XS Max" }, //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone SE",
+                devices: [
+                    { name: "iPhone SE" }, //TODO: Add image
+                    { name: "iPhone SE 2" }, //TODO: Add image
+                    { name: "iPhone SE 3" }, //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone 8",
+                devices: [
+                    { name: "iPhone 8" }, //TODO: Add image
+                    { name: "iPhone 8 Plus" } //TODO: Add image
+                ]
+            },
+            {
+                name: "iPhone 7",
+                devices: [
+                    { name: "iPhone 7" }, //TODO: Add image
+                    { name: "iPhone 7 Plus" } //TODO: Add image
+                ]
+            },
+          ]
+        },
+        {
+          name: "iPad",
+          image: "/assets/images/repair/Apple/iPad/Apple-iPad.png",
+          devices: [
+            { name: "iPad Pro" }, //TODO: Add image
+            { name: "iPad Air" }, //TODO: Add image
+            { name: "iPad" } //TODO: Add image
+          ]
+        },
+        {
+          name: "Apple Watch",
+          image: "/assets/images/repair/Apple/Watch/Apple-Watch.png",
+          devices: [
+            // It looks like these were mistakenly named as iPad models; consider updating these to Apple Watch models
+            { name: "Apple Watch Series 7" }, //TODO: Add image
+            { name: "Apple Watch SE" }, //TODO: Add image
+            // Add more Apple Watch models here
+          ]
+        }
+        //TODO: OTHER DEVICES
+      ]
+    },
+    {
+      brand: "Samsung",
+      image: "/assets/images/repair/Samsung/Samsung-Logo.svg",
+      models: [
+        {
+          name: "Galaxy",
+          // Add Galaxy series and devices here
+        }
+      ]
+    },
+    {
+      brand: "Google",
+      image: "/assets/images/repair/Google/Google-Logo.svg",
+      models: [
+        {
+          name: "Google Phone",
+          // Add Google Phone series and devices here
+        }
+      ]
+    },
+    {
+      brand: "LG",
+      image: "/assets/images/repair/LG/LG-Logo.svg",
+      models: [
+        {
+          name: "LG Phone",
+          // Add LG Phone series and devices here
+        }
+      ]
+    },
+    {
+      brand: "Huawei",
+      image: "/assets/images/repair/Huawei/Huawei-Logo.svg",
+      models: [
+        {
+          name: "Huawei Phone",
+          // Add Huawei Phone series and devices here
+        }
+      ]
+    },
+    {
+      brand: "Others",
+      image: "/assets/images/repair/Other/Other-Logo.png",
+      models: [
+        {
+          name: "Other Phones",
+          // Add Other Phone series and devices here
+        }
+      ]
+    }
+  ];
+  
+  export default deviceTypes;
+  
