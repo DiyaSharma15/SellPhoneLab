597b5dae42f312462eb808819a2a64bde824a301
sarbjeet brar <39264573+sarbjee@users.noreply.github.com>
2024-03-14 12:17:20 -0600

    accessories page work partial completed

--- app/ProductDetails/ProductDetails.js
+++ app/ProductDetails/ProductDetails.js
@@ -1,98 +1,153 @@
-import React from 'react'
-import { MdShoppingBag, MdShoppingBasket } from 'react-icons/md'
-import Link from 'next/link'
+import React from "react";
+import { MdShoppingBag, MdShoppingBasket } from "react-icons/md";
+import Link from "next/link";
 export default function ProductDetails() {
   return (
     <section className="py-5 productDetails">
-    <div className="container">
-      <div className="row gx-5">
-        <aside className="col-lg-6">
-          <div className="border rounded-4 mb-3 d-flex justify-content-center">
-            <div data-fslightbox="mygalley" className="rounded-4" target="_blank">
-              <img style={{maxWidth: "100%", maxHeight: "100vh", margin: "auto"}} className="rounded-4 fit" src="https://sellphonelab.ca/wp-content/uploads/2022/10/arm-band.jpg" />
-            </div>
-          </div>
-          <div className="d-flex justify-content-center mb-3">
-            <button data-fslightbox="mygalley" className="border mx-1 rounded-2" target="_blank" >
-              <img width="60" height="60" className="rounded-2" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big1.webp" />
-            </button>
-            <button data-fslightbox="mygalley" className="border mx-1 rounded-2" target="_blank" >
-              <img width="60" height="60" className="rounded-2" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big2.webp" />
-            </button>
-            <button data-fslightbox="mygalley" className="border mx-1 rounded-2" target="_blank" >
-              <img width="60" height="60" className="rounded-2" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big3.webp" />
-            </button>
-            <button data-fslightbox="mygalley" className="border mx-1 rounded-2" target="_blank" >
-              <img width="60" height="60" className="rounded-2" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big4.webp" />
-            </button>
-            <button data-fslightbox="mygalley" className="border mx-1 rounded-2" target="_blank" >
-              <img width="60" height="60" className="rounded-2" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big.webp" />
-            </button>
-          </div>
-    
-        </aside>
-        <main className="col-lg-6">
-          <div className="ps-lg-3">
-            <h4 className="title text-dark">
-              Quality Men's Hoodie for Winter, Men's Fashion <br />
-              Casual Hoodie
-            </h4>
-            <div className="d-flex flex-row my-3">
-             
-              <span className="text-muted"><MdShoppingBasket/> 154 orders</span>
-              <span className="text-success ms-2">In stock</span>
-            </div>
-  
-            <div className="mb-3">
-              <span className="h5">$75.00</span>
-              <span className="text-muted">/per box</span>
-            </div>
-  
-            <p>
-              Modern look and quality demo item is a streetwear-inspired collection that continues to break away from the conventions of mainstream fashion. Made in Italy, these black and brown clothing low-top shirts for
-              men.
-            </p>
-  
-            <div className="row">
-              <dt className="col-3">Type:</dt>
-              <dd className="col-9">Regular</dd>
-  
-              <dt className="col-3">Color</dt>
-              <dd className="col-9">Brown</dd>
-  
-              <dt className="col-3">Material</dt>
-              <dd className="col-9">Cotton, Jeans</dd>
-  
-              <dt className="col-3">Brand</dt>
-              <dd className="col-9">Reebook</dd>
-            </div>
-  
-            <hr />
-  
-            <div className="row mb-4">
-              
-         
-              <div className="col-md-4 col-6 mb-3">
-                <label className="mb-2 d-block">Quantity</label>
-                <div className="input-group mb-3" style={{width: "170px"}}>
-                <select style={{ width: "100px" }} className="form-select me-4">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
-
-                  
-                </div>
+      <div className="container">
+        <div className="row gx-5">
+          <aside className="col-lg-6">
+            <div className="border rounded-4 mb-3 d-flex justify-content-center">
+              <div
+                data-fslightbox="mygalley"
+                className="rounded-4"
+                target="_blank"
+              >
+                <img
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "100vh",
+                    margin: "auto",
+                  }}
+                  className="rounded-4 fit"
+                  src="https://sellphonelab.ca/wp-content/uploads/2022/10/arm-band.jpg"
+                />
               </div>
             </div>
-          
-            <Link href="/cart" className="btn btn-primary shadow-0"> <MdShoppingBag/> Add to cart </Link>
-            
-          </div>
-        </main>
+            <div className="d-flex justify-content-center mb-3">
+              <button
+                data-fslightbox="mygalley"
+                className="border mx-1 rounded-2"
+                target="_blank"
+              >
+                <img
+                  width="60"
+                  height="60"
+                  className="rounded-2"
+                  src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big1.webp"
+                />
+              </button>
+              <button
+                data-fslightbox="mygalley"
+                className="border mx-1 rounded-2"
+                target="_blank"
+              >
+                <img
+                  width="60"
+                  height="60"
+                  className="rounded-2"
+                  src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big2.webp"
+                />
+              </button>
+              <button
+                data-fslightbox="mygalley"
+                className="border mx-1 rounded-2"
+                target="_blank"
+              >
+                <img
+                  width="60"
+                  height="60"
+                  className="rounded-2"
+                  src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big3.webp"
+                />
+              </button>
+              <button
+                data-fslightbox="mygalley"
+                className="border mx-1 rounded-2"
+                target="_blank"
+              >
+                <img
+                  width="60"
+                  height="60"
+                  className="rounded-2"
+                  src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big4.webp"
+                />
+              </button>
+              <button
+                data-fslightbox="mygalley"
+                className="border mx-1 rounded-2"
+                target="_blank"
+              >
+                <img
+                  width="60"
+                  height="60"
+                  className="rounded-2"
+                  src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/detail1/big.webp"
+                />
+              </button>
+            </div>
+          </aside>
+          <main className="col-lg-6">
+            <div className="ps-lg-3">
+              <h4 className="title text-dark">Camera Lens Protector</h4>
+              <div className="d-flex flex-row my-3">
+                <span className="text-muted">
+                  <MdShoppingBasket /> 154 orders
+                </span>
+                <span className="text-success ms-2">In stock</span>
+              </div>
+
+              <div className="mb-3">
+                <span className="h5">$75.00</span>
+                <span className="text-muted">/per box</span>
+              </div>
+
+              <p>
+                The best camera lens protectors for the iPhone 13 Pro & iPhone
+                13 Pro Max in 2024
+              </p>
+
+              <div className="row">
+                <dt className="col-3">Type:</dt>
+                <dd className="col-9">Regular</dd>
+
+                <dt className="col-3">Color</dt>
+                <dd className="col-9">white</dd>
+
+                <dt className="col-3">Material</dt>
+                <dd className="col-9">info about the Material</dd>
+
+                <dt className="col-3">Brand</dt>
+                <dd className="col-9">Apple</dd>
+              </div>
+
+              <hr />
+
+              <div className="row mb-4">
+                <div className="col-md-4 col-6 mb-3">
+                  <label className="mb-2 d-block">Quantity</label>
+                  <div className="input-group mb-3" style={{ width: "170px" }}>
+                    <select
+                      style={{ width: "100px" }}
+                      className="form-select me-4"
+                    >
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <Link href="/cart" className="btn btn-primary shadow-0">
+                {" "}
+                <MdShoppingBag /> Add to cart{" "}
+              </Link>
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
--- app/accessories/product-categories/ProductCategories.js
+++ app/accessories/product-categories/ProductCategories.js
@@ -1,51 +1,54 @@
 "use client";
-import React,{useState} from 'react'
-import './productCategories.css'
-import ProductCategoriesItem from './product-categories-item/ProductCategoriesItem'
-import sofa from './Images/Lovepik_com-400681657-scandinavian-indoor-home-environment-sofa.png';
-import mattress from './Images/Lovepik_com-401109348-bed.png';
-import pillow from './Images/category-thumb-6-removebg-preview.png';
-import bedsheets from './Images/Bedsheets.png';
-import blind from './Images/pngwing.com.png';
-import wallpaper from './Images/wallpaper.png';
+import React, { useState } from "react";
+import "./productCategories.css";
+import ProductCategoriesItem from "./product-categories-item/ProductCategoriesItem";
+import phonecase from "./Images/phone-photos-9-1.jpg.webp";
+import PrivacyTemperedGlass from "./Images/Privacy-Tempered-Glass.webp";
+import ArmBand from "./Images/arm-band.jpg.webp";
+import MultiCountryWallPlug from "./Images/Multi-Country-Wall-Plug.webp";
+import WiredEarPlugs from "./Images/Wired-Ear-Plugs.jpg.webp";
+import AppleWatchProtector from "./Images/Apple-Watch-Protector.jpg (1).webp";
 
-
-const CategoriesItem=[
-    {
-      name:"Sofa",
-      image:sofa
-    },{
-        name:"Mattress",
-        image:mattress
-    },
-    {
-        name:"Pillow",
-        image:pillow
-    },{
-      name:"Bedsheets",
-      image:bedsheets
+const CategoriesItem = [
+  {
+    name: "Phone Cases",
+    image: phonecase,
   },
-    {
-        name:"Blind",
-        image:blind
-    },{
-        name:"Chair",
-        image:wallpaper
-    }
-]
+  {
+    name: "Privacy Tempered Glass",
+    image: PrivacyTemperedGlass,
+  },
+  {
+    name: "Arm Band",
+    image: ArmBand,
+  },
+  {
+    name: "Multi Country Wall Plug",
+    image: MultiCountryWallPlug,
+  },
+  {
+    name: "Wired Ear Plugs",
+    image: WiredEarPlugs,
+  },
+  {
+    name: "Apple Watch Protector",
+    image: AppleWatchProtector,
+  },
+];
 const ProductCategories = (props) => {
-
   return (
-
-    <div className='container flex-wrap d-flex flex-row my-5 categories-container-box align-items-center justify-content-between my-3'>
-
-
-    {CategoriesItem.map((element)=>{
-            return <ProductCategoriesItem key={element.name} name={element.name} image={element.image}/>
-        })}
-
+    <div className="container flex-wrap d-flex flex-row  categories-container-box align-items-center justify-content-between my-3">
+      {CategoriesItem.map((element) => {
+        return (
+          <ProductCategoriesItem
+            key={element.name}
+            name={element.name}
+            image={element.image}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ProductCategories
+export default ProductCategories;
