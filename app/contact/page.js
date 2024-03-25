7519974477cdde9907522d987131e2381d53a989
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-03-25 17:38:13 -0600

    Changing contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,17 +1,25 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AppointmentForm from "../components/AppointmentForm";
+import styles from "./ContactPage.module.css"
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <div className = "container">
-        <div className="globalText">Contact Us</div>
+      <div className = {styles.container}>
         <AppointmentForm />
+        <div className = {styles.content}>
+          <div className="globalText">Here at the Sellphone Lab, we offer high quality repairs, 
+          phones and accessories and our technicians are highly qualified. Visit our store today and 
+          let us inspect your phone. We are confident that you will be back!
+          </div>
+          
+        </div>
       </div>
       <Footer />
     </div>
