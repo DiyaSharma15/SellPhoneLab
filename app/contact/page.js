f0424ceedfd019a218f90661048cce042b2422b4
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-03-25 17:12:04 -0600

    Work on contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,15 +1,19 @@
-import React from "react";
-import Header from "../components/Header"
-import Footer from '../components/Footer';
+"use client";
+
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import AppointmentForm from "../components/AppointmentForm";
+
 export default function Contact() {
-    return (
-        <div>
-        <Header/>
-        <div>
-          
-        </div>
-        <Footer/>
-  
+  return (
+    <div>
+      <Header />
+      <div className = "container">
+        <div className="globalText">Contact Us</div>
+        <AppointmentForm />
       </div>
-    );
-    }
+      <Footer />
+    </div>
+  );
+}
