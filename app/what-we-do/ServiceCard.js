5c90d4135497125002f029f48553978fbc3ea83c
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-03-27 17:17:33 -0600

    Working on WhatWeDo page
    Trying to get styling of a single component working

--- app/what-we-do/ServiceCard.js
+++ app/what-we-do/ServiceCard.js
@@ -5,10 +5,10 @@ import Link from "next/link";
 import styles from './ServiceCard.module.css';
 
 //Function to accept serviceCard display options
-function ServiceCard({title, description, imageScr, readMoreLink}) {
+function ServiceCard({title, description, imageSrc, readMoreLink}) {
     return (
         <div className={styles.card}>
-            <Image src={imageScr} alt={title} width={100} height={100} />
+            <Image src={imageSrc} alt={title} width={100} height={100} />
             <h2>{title}</h2>
             <p>{description}</p>
             {readMoreLink && <Link href={readMoreLink}>Read more</Link>}
--- app/what-we-do/page.js
+++ app/what-we-do/page.js
@@ -1,21 +1,24 @@
 import React from "react";
-import Header from "../components/Header"
-import Footer from '../components/Footer';
-import ServiceCard from './ServiceCard'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ServiceCard from "./ServiceCard";
+import styles from "./ServiceCard.module.css";
 
 //What SellPhoneLabs does page
 export default function WhatWeDo() {
-    return (
-        <div>
-        <Header/>
+  return (
+    <div>
+      <Header />
+      <div className={styles.servicesContainer}>
         <ServiceCard
-          title = "Unlock Phones"
-          description = "Testing"
-          imageSrc="/assets/images/whatwedo/Unlock-Phones.jpg"
+          title="Unlock Phones"
+          description="Testing"
+          imageSrc="/assets/images/whatwedo/unlockphones.jpg"
           readMoreLink="/what-we-do"
         />
-        <Footer/>
       </div>
-    );
-    }
 
+      <Footer />
+    </div>
+  );
+}
