527312a8dd8a45ec25e16472edb27fefa0253b3c
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-03-27 15:55:24 -0600

    .

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,20 +5,29 @@ import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AppointmentForm from "../components/AppointmentForm";
-import styles from "./ContactPage.module.css"
+import styles from "./ContactPage.module.css";
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <div className = {styles.container}>
+      <div className={styles.container}>
         <AppointmentForm />
-        <div className = {styles.content}>
-          <div className="globalText">Here at the Sellphone Lab, we offer high quality repairs, 
-          phones and accessories and our technicians are highly qualified. Visit our store today and 
-          let us inspect your phone. We are confident that you will be back!
+        <div className={styles.content}>
+          <div className="globalText">
+            Here at the Sellphone Lab, we offer high quality repairs, phones and
+            accessories and our technicians are highly qualified. Visit our
+            store today and let us inspect your phone. We are confident that you
+            will be back!
           </div>
-          
+          <div>
+            <Image
+              src="/assets/images/about/mobile-phones-6-1024x576.jpg"
+              alt="Mobile Phones"
+              width={1024}
+              height={576}
+            />
+          </div>
         </div>
       </div>
       <Footer />
