f947cfb6b478e954fa9545805b7b432765716456
DiyaSharma15 <harry.diya161@gmail.com>
2024-03-28 15:48:53 -0600

    Checkout page placeholder value fixed

--- app/checkout/Checkout.js
+++ app/checkout/Checkout.js
@@ -87,7 +87,7 @@ export default function Checkout() {
                                     <div className="col-6 mb-3">
                                         <p className="mb-0">Phone</p>
                                         <div className="form-outline">
-                                            <input type="tel" id="typePhone" defaultValue="+48 " className="form-control" />
+                                            <input type="tel" id="typePhone" defaultValue="+1 " className="form-control" />
                                         </div>
                                     </div>
 
@@ -147,9 +147,15 @@ export default function Checkout() {
                                     <div className="col-sm-4 mb-3">
                                         <p className="mb-0">City</p>
                                         <select className="form-select">
-                                            <option value="1">New York</option>
-                                            <option value="2">Moscow</option>
-                                            <option value="3">Samarqand</option>
+                                            <option value="1">Calgary</option>
+                                            <option value="2">Edmonton</option>
+                                            <option value="3">High River</option>
+                                            <option value="4">Airdrie</option>
+                                            <option value="5">Okotoks</option>
+                                            <option value="6">Black Diamond</option>
+                                            <option value="7">Crossfield</option>
+                                            <option value="8">Irricana</option>
+
                                         </select>
                                     </div>
 
