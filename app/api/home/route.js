92aaebe1beeacce13ae38eb03aa6f03ccf40f1ed
sarbjeet brar <39264573+sarbjee@users.noreply.github.com>
2024-04-02 11:23:03 -0600

    testing stripe online

--- app/api/home/route.js
+++ app/api/home/route.js
@@ -1,54 +1,59 @@
-import { NextResponse } from 'next/server';
-import stripePackage from 'stripe';
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_SCRET);
+import { NextResponse } from "next/server";
+import stripePackage from "stripe";
+const stripe = require("stripe")(
+  "pk_test_51OrOfrP991lWFWFdIyZHvDhB2mPEgc94PgaEsatHJnHH1BZ5NsKYKvUExRHzA7D4GysVdMTXcMOgB8gIyz5m979c00SZkjWaUJ"
+);
 
+//const stripe = require("stripe")(`${process.env.NEXT_PUBLIC_SCRET}`);
 export async function POST(req, res) {
-    if (req.method === 'POST') {
-        try {
-            const body = await req.json();
-            const { items } = body;
-            
-            const transformedItems = items.map((item) => ({
-                price_data: {
-                    currency: "CAD",
-                    product_data: {
-                        name: item.name,
-                        images: [item.image],
-                    },
-                    unit_amount: Math.round(item.price * 100),
-                },
-                quantity: item.quantity,
-            }))
-           // console.log(transformedItems)
-            try {
-               
-                
-                const session = await stripe.checkout.sessions.create({
-                  line_items: transformedItems,
-                  mode: 'payment',
-                  success_url: 'http://localhost:3000/success',
-                  cancel_url: 'http://localhost:3000/',
-                }
-                // {
-                //     headers: stripeHeaders
-                // }
-                );
-                return NextResponse.json({"sessionURL": session.url});
-              } catch (err) {
-                console.log(err);
-                return NextResponse.status(err.statusCode || 500).json(err.message);
-              }
-            // You can add your processing logic here
+  if (req.method === "POST") {
+    try {
+      const body = await req.json();
+      const { items } = body;
 
-            // Return a success response if processing is successful
-           // return NextResponse.json({ message: 'Processing successful' });
-        } catch (error) {
-            console.error('Error processing items:', error);
-            // Return an error response if an error occurs
-            return NextResponse.error(new Error('An internal server error occurred'), { status: 500 });
-        }
-    } else {
-        // Return a method not allowed response for non-POST requests
-        return NextResponse.error(new Error('Method Not Allowed'), { status: 405 });
+      const transformedItems = items.map((item) => ({
+        price_data: {
+          currency: "CAD",
+          product_data: {
+            name: item.name,
+            images: [item.image],
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      }));
+      // console.log(transformedItems)
+      try {
+        const session = await stripe.checkout.sessions.create(
+          {
+            line_items: transformedItems,
+            mode: "payment",
+            success_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/",
+          }
+          // {
+          //     headers: stripeHeaders
+          // }
+        );
+        return NextResponse.json({ sessionURL: session.url });
+      } catch (err) {
+        console.log(err);
+        return NextResponse.status(err.statusCode || 500).json(err.message);
+      }
+      // You can add your processing logic here
+
+      // Return a success response if processing is successful
+      // return NextResponse.json({ message: 'Processing successful' });
+    } catch (error) {
+      console.error("Error processing items:", error);
+      // Return an error response if an error occurs
+      return NextResponse.error(
+        new Error("An internal server error occurred"),
+        { status: 500 }
+      );
     }
+  } else {
+    // Return a method not allowed response for non-POST requests
+    return NextResponse.error(new Error("Method Not Allowed"), { status: 405 });
+  }
 }
