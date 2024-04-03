d5f635629dfa7687123e1274bcfb352586430a67
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2024-04-03 14:23:50 -0600

    Continue working in appointment page

--- app/admin/appointments/page.js
+++ app/admin/appointments/page.js
@@ -6,6 +6,25 @@ import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 import Header from '../../components/Header'; // Adjust the path as necessary
 import Footer from '../../components/Footer'; 
 import styles from '../_components/appointments.module.css';
+import deviceTypes from '../../repair/deviceData'; // Adjust the import path as necessary
+
+
+const findDeviceImageUrl = (deviceModel) => {
+    for (const brand of deviceTypes) {
+      for (const model of brand.models) {
+        for (const series of model.series) {
+          for (const device of series.devices) {
+            if (device.name === deviceModel) {
+              return device.image; // Returns the image URL of the matched model
+            }
+          }
+        }
+      }
+    }
+    return null; // Return null or a default image if not found
+  };
+  
+
 
 const Appointments = () => {
     const [appointments, setAppointments] = useState([]);
@@ -28,29 +47,40 @@ const Appointments = () => {
         <>
             <Header />
             <div className={styles.container}>
-                <h1 className={styles.title}>Appointments</h1>
+                
                 <div className={styles.appointmentsLayout}>
+                    
                     <ul className={styles.appointmentList}>
+                    <div className="globalTitle">Appointments</div>
                         {appointments.map(appointment => (
                             <li key={appointment.id} className={styles.appointmentItem} onClick={() => selectAppointment(appointment)}>
                                 {/* <p>Name: {appointment.firstName} {appointment.lastName}</p> */}
-                                <p>Email: {appointment.email}</p>
-                                <p>Device Model: {appointment.deviceModel}</p>
-                                <p>Services: {appointment.repairTypes ? appointment.repairTypes.join(', ') : 'No services listed'}</p>
+                                <p><strong>Store: </strong> {appointment.location}</p>
+                                {/* <p><strong>Email:</strong> {appointment.email}</p> */}
+                                {/* <p><strong>Device Model:</strong> {appointment.deviceModel}</p> */}
+                                <p><strong>Services:</strong> {appointment.repairTypes ? appointment.repairTypes.join(', ') : 'No services listed'}</p>
+                                <p><strong>Date/Time: </strong>{appointment.startDateTime?.toDate().toLocaleString()}</p>
                                 {/* <p>Issue: {appointment.issueDescription}</p>
                                 <p>Submitted At: {appointment.submittedAt?.toDate().toLocaleString()}</p> */}
                             </li>
                         ))}
                     </ul>
                     {selectedAppointment && (
+                        <>
                         <div className={styles.appointmentDetails}>
-                            <h2>Details</h2>
-                            <p>Name: {selectedAppointment.firstName} {selectedAppointment.lastName}</p>
-                            <p>Email: {selectedAppointment.email}</p>
-                            <p>Device Model: {selectedAppointment.deviceModel}</p>
-                            <p>Issue: {selectedAppointment.issueDescription}</p>
-                            <p>Submitted At: {selectedAppointment.submittedAt?.toDate().toLocaleString()}</p>
+                        <div className = "globalTitle">Details</div>
+                            <p><strong>Name: </strong>{selectedAppointment.firstName} {selectedAppointment.lastName}</p>
+                            <p><strong>Email: </strong>{selectedAppointment.email}</p>
+                            <p><strong>Device Model: </strong>{selectedAppointment.deviceModel}</p>
+                            <img src={findDeviceImageUrl(selectedAppointment.deviceModel)} alt={selectedAppointment.deviceModel} style={{ width: '250px', height: '250px' }} />
+                            <p><strong>Services:</strong> {selectedAppointment.repairTypes ? selectedAppointment.repairTypes.join(', ') : 'No services listed'}</p>
+                            <p><strong>Notes: </strong>{selectedAppointment.issueDescription}</p>
+                            <p><strong>Store: </strong>{selectedAppointment.location}</p>
+                            <p><strong>Appointment Date/Time: </strong>{selectedAppointment.startDateTime?.toDate().toLocaleString()}</p>
+                            {/* <p>Submitted At: {selectedAppointment.submittedAt?.toDate().toLocaleString()}</p> */}
+                            
                         </div>
+                        </>
                     )}
                 </div>
             </div>
