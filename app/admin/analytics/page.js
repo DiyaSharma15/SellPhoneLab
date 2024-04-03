637e3d861fdef397f311ea39909b0accb9242e5d
Seth_Dijkstra <119450434+SethDijkstra@users.noreply.github.com>
2024-04-03 13:34:42 -0600

    Updated to latest react version, added analytics option in admin dashboard

---
+++ app/admin/analytics/page.js
@@ -0,0 +1,13 @@
+// pages/admin/analytics.js
+import React from 'react';
+
+const AnalyticsPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Analytics</h1>
+      {/* Analytics components will go here */}
+    </div>
+  );
+};
+
+export default AnalyticsPage;
