7dd86423807adc2401d134a7c4e5ddd893ea9590
Seth_Dijkstra <seth.dijkstra@edu.sait.ca>
2024-04-03 18:54:33 -0600

    Changed one word for test deployment

--- app/admin/analytics/page.jsx
+++ app/admin/analytics/page.jsx
@@ -1,13 +1,11 @@
-// Assuming this file is located at /app/admin/analytics/page.jsx
-
-"use client"; // Next.js 14+ indicator for client components
+"use client"; 
 
 import React, { useEffect, useState } from 'react';
 import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from '/firebase-config'; // Correct path to your Firebase config
-import styles from '../_components/Analytics.module.css'; // Correct path to your CSS
-import Header from "../../components/Header"; // Correct path to your Header component
-import Footer from "../../components/Footer"; // Correct path to your Footer component
+import { db } from '/firebase-config'; 
+import styles from '../_components/Analytics.module.css'; 
+import Header from "../../components/Header"; 
+import Footer from "../../components/Footer"; 
 
 const Analytics = ({ stripeAnalytics = {} }) => {
   console.log('Received stripeAnalytics props:', stripeAnalytics);
@@ -45,10 +43,9 @@ const Analytics = ({ stripeAnalytics = {} }) => {
                   <p>{stripeAnalytics.totalTransactions || 0}</p>
               </div>
               <div>
-                  <h2>Total Revenue</h2>
+                  <h2>Total Revenue Made</h2>
                   <p>${stripeAnalytics.totalRevenue?.toFixed(2) || '0.00'}</p>
               </div>
-              {/* Additional analytics data display */}
               <div>
                   <h2>Appointments</h2>
                   <p>Completed Appointments: {completedAppointments}</p>
