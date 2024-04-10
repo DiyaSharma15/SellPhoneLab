254534088d7e38d4ced6a139155c0c680d58cdfe
Seth_Dijkstra <119450434+SethDijkstra@users.noreply.github.com>
2024-04-10 16:17:10 -0600

    Fixed images on blog page

--- pages/blog/blogPostCard.js
+++ pages/blog/blogPostCard.js
@@ -3,7 +3,7 @@ import styles from './BlogPostCard.module.css';
 function BlogPostCard({ post }) {
   return (
     <div className={styles.card}>
-      {post.imageUrl && <img src={post.imageUrl} className={styles.cardImgTop} alt={post.title} />}
+      {post.image && <img src={post.image} className={styles.cardImgTop} alt={post.title} />}
       <div className={styles.cardBody}>
         <h5 className={styles.cardTitle}>{post.title}</h5>
         <p className={styles.cardText}>{post.summary}</p>
